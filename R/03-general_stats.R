e9f3193849bfe793a57727c826248c5e78f8f578
hepplerj <jason.heppler@gmail.com>
2019-08-02 11:12:29 -0500

    Adding stats on women authors as percentage of all studies

--- R/03-general_stats.R
+++ R/03-general_stats.R
@@ -3,9 +3,27 @@
 # Count genders
 gender_predicted %>%
   group_by(gender) %>%
-  tally %>%
-  arrange(desc(n)) %>%
-  head(10)
+  tally
+
+# Total percentage of female authors among all authors
+(gender_predicted %>% filter(gender == "female") %>% tally) / (nrow(gender_predicted)) * 100 
+
+# Find any study where women are author
+female_authors_all <- gender_predicted %>% 
+  filter(gender == "female") %>% 
+  select(author)
+
+female_authors_all <- unique(female_authors_all$author)
+female_authors_all <- female_authors_all %>% as.data.frame()
+names(female_authors_all) <- "author"
+
+female_authors_collapsed <- female_authors_all %>% 
+  rowwise %>% 
+  ungroup %>%
+  { paste(.$author, collapse = "|") }
+
+list_authors_female <- data %>% filter(grepl(female_authors_collapsed, author))
+nrow(list_authors_female) / nrow(data) * 100
 
 # Top journals
 journals %>%
